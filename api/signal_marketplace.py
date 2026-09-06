d64f3c247dd219bdf867ec12cbd477505af5760a
yaya1738 <65517364+yaya1738@users.noreply.github.com>
2026-09-06 21:22:14 +0300

    Security: remove missed Telegram credential fallback

--- api/signal_marketplace.py
+++ api/signal_marketplace.py
@@ -322,7 +322,7 @@ def notify_capital_ready(amount):
     import os
     import requests
 
-    token = os.getenv('TELEGRAM_BOT_TOKEN', '8214203655:AAGkAamvjQq0b7T7lmaTPDd-yYY_hvo_xvA')
+    token = os.getenv('TELEGRAM_BOT_TOKEN', '')
     chat_id = os.getenv('TELEGRAM_CHAT_ID', '8327766663')
 
     message = f'''🎉 <b>CAPITAL ACQUIRED AUTONOMOUSLY</b>
